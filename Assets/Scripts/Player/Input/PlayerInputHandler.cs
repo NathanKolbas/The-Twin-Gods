2a17f043c9989db4cf483c7cd45965f627d07cf9
NathanKolbas <nathan2000@outlook.com>
2020-09-17 00:50:35 -0500

    Added VERY basic movment

---
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [SerializeField] [Range(0.0f, 100.0f)] private float speed = 50.0f;
+    
+    // private Rigidbody _rigidbody;
+    private Vector2 _movement;
+    
+    // private void Start()
+    // {
+    //     _rigidbody = GetComponent<Rigidbody>();
+    // }
+    
+    private void FixedUpdate()
+    {
+        // _rigidbody.AddForce(_movement * speed);
+        transform.Translate(_movement);
+    }
+    
+    
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        Vector2 movementInput = speed * context.ReadValue<Vector2>();
+        _movement = new Vector2(movementInput.x, movementInput.y);
+        _movement *= Time.deltaTime;
+    }
+}
