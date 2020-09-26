0deb3638f992502638e7c1e51b0927f4ab0bb562
Ben Laube <blaube2@huskers.unl.edu>
2020-09-26 13:44:33 -0500

    added edge and wall detection to enemy in scene 2

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -8,16 +8,17 @@ public class EnemyPatrol : MonoBehaviour
     public float moveSpeed;
     public bool moveRight;
 
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool onWall;
+
+    private bool notAtEdge;
+    public Transform edgeCheck;
+
     // Start is called before the first frame update
     void Start()
     {
-
-     if (moveRight) {
-         GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-     }
-     else{
-         GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-     }
 
     }
 
@@ -25,5 +26,23 @@ public class EnemyPatrol : MonoBehaviour
     void Update()
     {
         
+        onWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+        if (onWall || !notAtEdge) {
+            moveRight = !moveRight;
+        }
+
+        if (moveRight) {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else{
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+     }
+
+
     }
 }
