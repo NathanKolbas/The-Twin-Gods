46cdb62e253d95a0faa4d6aa531c0b5aae742cef
Ben Laube <blaube2@huskers.unl.edu>
2020-09-25 20:09:21 -0500

    added basic horizontal and vertical movement

---
+++ Assets/HorizontalMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour {
+    public float min = -5f;
+    public float max = 5f;
+    public float speed = 5f;
+
+    // Use this for initialization
+    void Start () {
+
+        min = transform.position.x + min;
+        max = transform.position.x + max;
+        speed = 5f;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+       transform.position = new Vector3(Mathf.PingPong(Time.time * speed, max - min) + min, transform.position.y, transform.position.z); 
+    
+    }
+       
+}
