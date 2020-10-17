3b1515e9a50a9c5d358f169d39d0bbe1088047f8
NathanKolbas <nathan2000@outlook.com>
2020-10-17 18:31:46 -0500

    Added Artemis idle animation

--- Assets/Scripts/Player/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -4,14 +4,16 @@ using UnityEngine.InputSystem;
 public class PlayerInputHandler : MonoBehaviour
 {
     [SerializeField] [Range(0.0f, 100.0f)] private float speed = 50.0f;
-    
+
+    private Animator _animator;
     // private Rigidbody _rigidbody;
     private Vector2 _movement;
-    
-    // private void Start()
-    // {
-    //     _rigidbody = GetComponent<Rigidbody>();
-    // }
+    private static readonly int X = Animator.StringToHash("x");
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
     
     private void FixedUpdate()
     {
@@ -25,5 +27,11 @@ public class PlayerInputHandler : MonoBehaviour
         Vector2 movementInput = speed * context.ReadValue<Vector2>();
         _movement = new Vector2(movementInput.x, movementInput.y);
         _movement *= Time.deltaTime;
+        AnimateMovment(movementInput);
+    }
+    
+    private void AnimateMovment(Vector2 direction) 
+    {
+        _animator.SetFloat(X, direction.x);
     }
 }
