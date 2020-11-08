5a2981db5ae5ea3f76c90a5714cd43f041acb993
NathanKolbas <nathan2000@outlook.com>
2020-11-08 01:57:56 -0600

    Added Artemis health-bar

---
+++ Assets/Scripts/Player/Artemis.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artemis : MonoBehaviour
+{
+    
+    [SerializeField] private Stat health;
+    [SerializeField] private float startingHealth = 100;
+    [SerializeField] private float maxHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health.Initialize(startingHealth, maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
