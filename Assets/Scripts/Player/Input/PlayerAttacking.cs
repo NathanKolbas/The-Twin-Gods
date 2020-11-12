3fdbb8376755c3e895249598eb499f5ff1155e02
NathanKolbas <nathan2000@outlook.com>
2020-11-11 18:27:53 -0600

    Added attack stab animation and flipping. Also started working on welcome screen.

--- Assets/Scripts/Player/Input/PlayerAttacking.cs
+++ Assets/Scripts/Player/Input/PlayerAttacking.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttacking : MonoBehaviour
 {
-    public void AttackingHandler()
+    public void Punch(bool facingLeft)
     {
         gameObject.SetActive(true);
+        var flipX = facingLeft ? -1 : 1;
+        gameObject.transform.localPosition = new Vector3(
+            flipX * Math.Abs(gameObject.transform.localPosition.x),
+            gameObject.transform.localPosition.y,
+            gameObject.transform.localPosition.z
+        );
         StartCoroutine(_punch(0.5f));
     }
 
--- Assets/Scripts/Player/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -1,26 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
     [SerializeField] [Range(0.0f, 100.0f)] private float speed = 50.0f;
-
-    private Animator _animator;
+    
     // private Rigidbody _rigidbody;
     private Vector2 _movement;
+    private SpriteRenderer _spriteRenderer;
+    public PlayerAttacking playerAttacking;
+    private bool _isAttacking = false;
+    private Animator _animator;
     private static readonly int X = Animator.StringToHash("x");
     private const int IdleLayer = 0;
     private const int WalkLayer = 1;
+    private const int AttackStab = 2;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
     
     private void FixedUpdate()
     {
         // _rigidbody.AddForce(_movement * speed);
-        transform.Translate(_movement);
+        if (!_isAttacking)
+        {
+            transform.Translate(_movement);
+        }
     }
     
     
@@ -37,11 +46,37 @@ public class PlayerInputHandler : MonoBehaviour
         {
             _animator.SetLayerWeight(WalkLayer, 0);
         }
+
+        if (movementInput.x > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (movementInput.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
     }
     
     private void AnimateMovment(Vector2 direction) 
     {
         _animator.SetLayerWeight(WalkLayer, 1);
         _animator.SetFloat(X, direction.x);
+    }
+    
+    public void AttackingHandler(InputAction.CallbackContext context)
+    {
+        _isAttacking = true;
+        playerAttacking.Punch(_spriteRenderer.flipX);
+        var attackLength = _animator.runtimeAnimatorController.animationClips[AttackStab].length;
+        StartCoroutine(_animateStabAttack(attackLength));
+    }
+    
+    private IEnumerator _animateStabAttack(float seconds)
+    {
+        _animator.SetLayerWeight(AttackStab, 1);
+        _animator.Play("Stab", AttackStab, 0f);
+        yield return new WaitForSeconds(seconds);
+        _animator.SetLayerWeight(AttackStab, 0);
+        _isAttacking = false;
     }
 }
